a2dd503a62185d9dd55ae186a4e27b140129819d
Lane Rettig <lanerettig@gmail.com>
2020-04-30 16:47:32 -0400

    Remove irrelevant things

--- index.js
+++ index.js
@@ -14,17 +14,11 @@ const GhostAdminApi = require('@tryghost/admin-api');
 
         const basePath = process.env.GITHUB_WORKSPACE;
         const pkgPath = path.join(process.env.GITHUB_WORKSPACE, 'package.json');
-        const themeName = core.getInput('theme-name') || require(pkgPath).name;
-        const themeZip = `${themeName}.zip`;
-        const zipPath = path.join(basePath, themeZip);
-        const exclude = core.getInput('exclude') || '';
-
-        // Create a zip
-        await exec.exec(`zip -r ${themeZip} . -x *.git* *.zip yarn* npm* *routes.yaml *redirects.yaml *redirects.json ${exclude}`, [], {cwd: basePath});
+        console.log(`Package path: ${pkgPath}`);
 
         // Deploy it to the configured site
-        await api.themes.upload({file: zipPath});
-        console.log(`${themeZip} successfully uploaded.`);
+        //await api.themes.upload({file: zipPath});
+        console.log(`Content successfully uploaded.`);
     } catch (err) {
         console.error(err);
         process.exit(1);
